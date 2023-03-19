a66200767f3c472a684e990ac02bd18b8a1104ee
ulranh <ulrich.anhalt@gmail.com>
2023-03-19 19:31:43 +0100

    1415

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "ulli 1213")
+	fmt.Fprintf(w, "ulli 1415")
 }
 
 func loggingMiddleware(next http.Handler) http.Handler {
