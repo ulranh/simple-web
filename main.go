f1169e5b060a145f939a965281f881fead755ca1
Ulrich Anhalt <ulrich.anhalt@gmail.com>
2023-11-29 16:11:07 +0100

    Update main.go add stop handler

--- main.go
+++ main.go
@@ -9,11 +9,16 @@ import (
 
 func main() {
 	http.Handle("/", loggingMiddleware(http.HandlerFunc(handler)))
+	http.Handle("/stop", http.HandlerFunc(stopHandler)))
 	http.ListenAndServe(":8000", nil)
 }
 
 func handler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "ulli 1415")
+}
+
+func stopHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "STOP!")
 }
 
 func loggingMiddleware(next http.Handler) http.Handler {
